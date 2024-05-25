b1a1c8fc5bfa1524826879f55793fa3e9bcac982
Jason_Hsu <9832660@gmail.com>
2024-05-25 17:10:05 +0800

    data_transformation complete

--- Xray/constant/training_pipeline/__init__.py
+++ Xray/constant/training_pipeline/__init__.py
@@ -7,13 +7,67 @@ TIMESTAMP: datetime = datetime.now().strftime("%m_%d_%Y_%H_%M_%S")
 
 ####################################
 
+
+## Data Ingestion Constants
 ARTIFACT_DIR: str = "artifact"
 
 BUCKET_NAME: str = "testing0524"
 
-S3_DATA_FOLDER: str = "test"
+S3_DATA_FOLDER: str = "data"
+
+
+### Data transformation constants
 
 CLASS_LABEL_1: str="NORMAL"
 
 CLASS_LABEL_2: str="PNEUMONIA"
+
+BRIGHTNESS: float = 0.10
+
+CONTRAST: float = 0.10
+
+SATURATION: float = 0.10
+
+HUE: float = 0.10
+
+RESIZE: int = 224
+
+CENTERCROP: int = 224
+
+RANDOMROTATION: int = 10
+
+NORMALIZE_LIST_1: List[float] = [0.485, 0.456, 0.406]
+
+NORMALIZE_LIST_2: List[float] = [0.229, 0.224, 0.225]
+
+TRAIN_TRANSFORMS_KEY: str = "xray_train_transforms"
+
+TRAIN_TRANSFORMATION_FILE: str = "train_transforms.pkl"
+
+TEST_TRANSFORMS_FILE: str = "test_transforms.pkl"
+
+BATCH_SIZE: int = 2
+
+SHUFFLE: bool = False
+
+PIN_MEMORY: bool = True
+
+
+#model trainer constants
+
+TRAINED_MODEL_DIR: str = "trained_model"
+
+TRAINED_MODEL_NAME: str = "model.pt"
+
+DEVICE: torch.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+STEP_SIZE: int = 6
+
+GAMMA: int = 0.5
+
+EPOCH: int = 1
+
+
+
+
 
--- Xray/entity/artifact_entity.py
+++ Xray/entity/artifact_entity.py
@@ -5,3 +5,11 @@ from torch.utils.data import DataLoader
 class DataIngestionArtifact:
     train_data_path: str
     test_data_path: str
+    
+    
+@dataclass
+class DataTransformationArtifact:
+    transformed_train_object: DataLoader
+    transformed_test_object: DataLoader
+    train_transform_file_path: str
+    test_transform_file_path: str
